13ad3dec29adccd369626c7e8f0f1cd595e139f3
Serenity Buckner <scbuckn2@ncsu.edu>
2026-03-21 10:19:17 -0400

    Day 6: Contact Form

--- montyfriends/app/(4)contact/contact.tsx
+++ montyfriends/app/(4)contact/contact.tsx
@@ -23,70 +23,102 @@ import Image from 'next/image';
 import { ToastContainer, toast } from 'react-toastify';
 
 export default function Contact(){
-    
-   {/*Empty Form*/}
+
+    {/*Empty Form*/}
     const initialFormData = {
         name: "",
-        email: "",
+        companyName: "",
+        industry: "",
         subject: "",
+        email: "",
         message: "",
+        annualRev: "",
     };
 
-    const [formData, setFormData] = useState(initialFormData);
+    const [formDataServer, setFormDataServer] = useState(initialFormData);
+    const [result, setResult] = useState("");
 
     {/*Update Input Value*/}
     const handleChange = (event: any) => {
-        setFormData({...formData, [event.target.name]: event.target.value})
+        setFormDataServer({...formDataServer, [event.target.name]: event.target.value})
     }
 
-    {/*Send to personal Email*/}
-    const handleSubmit = async(event:any) => {
-        {/*Dont let field be empty*/}
+    const onSubmit = async (event: any) => {
         event.preventDefault();
-
+        
         {/* Grab HTML Elements */}
         const nameError = document.getElementById("nameError") as HTMLElement;
+        const compNameError = document.getElementById("compNameError") as HTMLElement;
+        const industryError = document.getElementById("industryError") as HTMLElement;
+        const subjectError = document.getElementById("subjectError") as HTMLElement;
         const emailError = document.getElementById("emailError") as HTMLElement;
         const messageError = document.getElementById("messageError") as HTMLElement;
+        const annualRevError = document.getElementById("annualRevError") as HTMLElement;
+
 
         {/*Valid Email*/}
         const emailErrorB = document.getElementById("inputB") as HTMLInputElement; 
         const validEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; 
 
-        if(!formData.name || !emailErrorB.value.match(validEmail) || !formData.message){
+        if(!formDataServer.name || !formDataServer.email.match(validEmail) || !formDataServer.message ||
+           !formDataServer.industry || !formDataServer.companyName || !formDataServer.subject || 
+           !formDataServer.annualRev){
             {/*If field is not valid, add error */}
-            if(!formData.name) {nameError.innerHTML = "* Please insert your name."};
-            if(!emailErrorB.value.match(validEmail)){emailError.innerHTML = "* Please insert a valid email."}
-            if(!formData.message){ messageError.innerHTML = "* Please type your message."};
+            if(!formDataServer.name) {nameError.innerHTML = "* Please insert your name."};
+            if(!formDataServer.companyName) {compNameError.innerHTML = "* Please insert the company name."};
+            if(!formDataServer.industry) {industryError.innerHTML = "* Please specify the industry you're in."};
+            if(!formDataServer.subject) {subjectError.innerHTML = "* Please clarify the subject you need help with."};
+            if(!formDataServer.email.match(validEmail)){emailError.innerHTML = "* Please insert a valid email."}
+            if(!formDataServer.message){ messageError.innerHTML = "* Please type a complete message."};
+            if(!formDataServer.annualRev){ annualRevError.innerHTML = "* Please insert a valid revenue amount."};
 
             {/*If field is now valid, remove error */}
-            if(formData.name) {nameError.innerHTML = ""};
-            if(emailErrorB.value.match(validEmail)){emailError.innerHTML = ""};
-            if(formData.message){messageError.innerHTML = ""};
+            if(formDataServer.name) {nameError.innerHTML = ""};
+            if(formDataServer.companyName) {compNameError.innerHTML = ""};
+            if(formDataServer.industry) {industryError.innerHTML = ""};
+            if(formDataServer.subject) {subjectError.innerHTML = ""};
+            if(formDataServer.email.match(validEmail)){emailError.innerHTML = ""}
+            if(formDataServer.message){ messageError.innerHTML = ""};
+            if(formDataServer.annualRev){ annualRevError.innerHTML = ""};
 
             toast.error('Please check all input fields.');
             return false;
-        }
+           }
 
-        {/*Make sure all errors are removed*/}
-        nameError.innerHTML = "";
-        emailError.innerHTML = "";
-        messageError.innerHTML = "";
+            {/*Make sure all errors are removed*/}
+            nameError.innerHTML = "";
+            compNameError.innerHTML = "";
+            industryError.innerHTML = "";
+            subjectError.innerHTML = "";
+            emailError.innerHTML = "";
+            messageError.innerHTML = "";
+            annualRevError.innerHTML = "";
 
-        try{
-            const response = await fetch('/api/send', {
-                method: 'POST',
-                body: JSON.stringify({...formData}),
-            });
-        /*Email Sent !*/
-            setFormData(initialFormData);
+            setResult("Sending....");
+            const formData = new FormData(event.target);
+            formData.append("access_key", "d6792353-b77e-4f33-a38f-f9dfbd338b2d");
+            // d6792353-b77e-4f33-a38f-f9dfbd338b2d
+            const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+        
+        const data = await response.json();
+
+        if (data.success) {
+            setFormDataServer(initialFormData);
+            setResult("Form Submitted Successfully");
             toast.success("Email was sent sucessfully!");
-        } catch(error:any){
-        /*Email Error !*/
+            event.target.reset();
+        } else {
             
-            toast.error('Oops, looks like something went wrong ', error);
+            toast.error('Oops, looks like something went wrong ');
+            
+            setResult("Error");
         }
+
     };
+
 
     return(
         <div className='section' id = "contactSection">
@@ -104,7 +136,7 @@ export default function Contact(){
                 </p>
                 {/*Form*/}
                 <div className = "formBox">
-                    <form name = "conctactForm" onSubmit = {handleSubmit} className='form' >
+                    <form name = "conctactForm" onSubmit = {onSubmit} className='form' >
                         <fieldset className='formField'>
                             <h2 id='formSubTitles' className = {ralewayBold.className} >NAME</h2>
                             <div className = {ralewayBold.className} id = "nameError"></div>
@@ -113,38 +145,38 @@ export default function Contact(){
                                 id='inputA' 
                                 type = 'text'
                                 name = 'name'
-                                value = {formData.name}
-                                onChange={handleChange}/>
+                                onChange={handleChange}
+                                />
 
                             <h2 id='formSubTitles' className = {ralewayBold.className} >COMPANY NAME</h2>
-                            <div className = {ralewayBold.className} id = ""></div>
+                            <div className = {ralewayBold.className} id = "compNameError"></div>
                                 <input 
                                 className= {raleway.className} 
                                 id='inputA' 
                                 type = 'text'
-                                name = 'name'
-                                value = {formData.name}
-                                onChange={handleChange}/>
+                                name = 'companyName'
+                                onChange={handleChange}
+                                />
 
                             <h2 id='formSubTitles' className = {ralewayBold.className} >INDUSTRY</h2>
-                            <div className = {ralewayBold.className} id = ""></div>
+                            <div className = {ralewayBold.className} id = "industryError"></div>
                                 <input 
                                 className= {raleway.className} 
                                 id='inputA' 
                                 type = 'text'
-                                name = 'name'
-                                value = {formData.name}
-                                onChange={handleChange}/>
+                                name = 'industry'
+                                onChange={handleChange}
+                                />
 
                             <h2 id='formSubTitles' className = {ralewayBold.className}>SUBJECT</h2>
-                            <div className = {ralewayBold.className} id = ""></div>
+                            <div className = {ralewayBold.className} id = "subjectError"></div>
                                 <input  
                                 className= {raleway.className} 
-                                id='inputB'
+                                id='inputA'
                                 type = 'text' 
-                                name = 'email'
-                                value = {formData.email} 
-                                onChange={handleChange}/>
+                                name = 'subject'
+                                onChange={handleChange}
+                                />
 
                             <h2 id='formSubTitles' className = {ralewayBold.className}>EMAIL</h2>
                             <div className = {ralewayBold.className} id = "emailError"></div>
@@ -153,8 +185,8 @@ export default function Contact(){
                                 id='inputB'
                                 type = 'text' 
                                 name = 'email'
-                                value = {formData.email} 
-                                onChange={handleChange}/>
+                                onChange={handleChange}
+                                />
 
                             <h2 id='formSubTitles' className = {ralewayBold.className}>MESSAGE</h2>
                             <div className = {ralewayBold.className} id = "messageError"></div>
@@ -162,18 +194,17 @@ export default function Contact(){
                                 className= {raleway.className} 
                                 id='textArea' 
                                 name = 'message' 
-                                value = {formData.message} 
                                 onChange={handleChange}>
                             </textarea>
 
                             <h2 id='formSubTitles' className = {ralewayBold.className}>ANNUAL REVENUE RANGE</h2>
-                            <div className = {ralewayBold.className} id = "messageError"></div>
+                            <div className = {ralewayBold.className} id = "annualRevError"></div>
                             <textarea  
                                 className= {raleway.className} 
                                 id='annualRev' 
-                                name = 'message' 
-                                value = {formData.message} 
-                                onChange={handleChange}>
+                                name = 'annualRev'
+                                onChange={handleChange}
+                                >
                             </textarea>
 
                         </fieldset>
